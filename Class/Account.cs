4031a3f9fbdd11ad54d2c4dce19d8b3fb38d71a5
unknown <mirzaab@code.edu.az>
2022-12-14 03:51:05 +0400

    Class

---
+++ Class/Account.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+
+
+//namespace Class
+//{
+//    internal class Account
+//    {
+
+//        public string userName;
+//        public string password;
+
+//        public void login()
+//        {
+//            if (userName == "cavid123" && password == "Cavid1993")
+//            {
+//                Console.WriteLine("Giris Uqurludur");
+
+//            }
+//            else
+            
+//            {
+               
+//                Console.WriteLine("User name ve ya Parol sefdi");
+//            }
+
+
+//        }
+
+
+
+
+//    }
+//}
---
+++ Class/Program.cs
@@ -0,0 +1,45 @@
+﻿#region Praktika
+
+
+//string name = "Bul";
+//string name2 = "Bule";
+//string endName = name + name2;
+
+//Console.Write(endName);
+
+
+//------------------------------
+
+
+
+
+//string name1 = "Mirze";
+//string surname1 = "Beshirzade";
+//int age1 = 26;
+
+//string name2 = "Hemze";
+//string surname2 = "Alizade";
+//int age2 = 24;
+
+//Console.WriteLine($"{name1} {surname1} {age1}");
+
+//Console.WriteLine($"{name2} {surname2} {age2}");
+
+
+//---------------------------------------------------------
+
+
+
+#endregion
+
+
+
+
+
+//using Class;
+
+//Account Account =new Account();
+//Account.userName = "cavi123";
+//Account.password = "Cavid1993";
+
+//Account.login();
